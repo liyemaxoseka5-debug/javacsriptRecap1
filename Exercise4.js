fb21a3d0ba7222d3101bcc9070dd95e71abde231
liyema xoseka <liyemaxoseka5@gmail.com>
2026-05-26 09:45:42 +0200

    :Apple:

---
+++ Exercise4.js
@@ -0,0 +1,23 @@
+//Liyema Xoseka and Busiswa Bala
+
+
+// TODO: Create a variable called score and give it a value between 0 and 100
+let score = 70;
+// TODO: Create a variable called passingGrade and set it to 60
+let passingGrade = 60;
+// TODO: Write an if statement to check if score is greater than or equal to passingGrade
+if (score >= passingGrade) {
+    console.log ("Passed");
+    
+}
+// TODO: Add an else statement for when the score is below passingGrade
+else { console.log ("Failed");
+}
+// TODO: Print "Passed" or "Failed" based on the score
+ console.log(score >= passingGrade ? "Passed" : "Failed");
+// TODO: Add another condition to check if the score is perfect (100)
+if (score == 100) {
+    console.log("Perfect score!");
+}
+else 
+    console.log(("This is not a perfect score."));
